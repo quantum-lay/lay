37f5fa5dca61971384a3115287f12931d0dac5e2
gyu-don <takumi.kt+git@gmail.com>
2021-01-03 19:08:08 +0900

    parametric operation

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,20 @@ use std::fmt::Debug;
 pub mod gates;
 pub mod operations;
 
-pub use gates::{PauliGate, HGate, SGate, TGate, CXGate};
-pub use operations::{Operation, OpsVec};
-
-pub trait Layer {
+pub trait Layer<Operation = operations::Operation<Self>> {
     type Qubit: Debug;
     type Slot: Debug;
     type Buffer;
     type Requested;
     type Response;
 
-    fn send(&mut self, ops: &[Operation<Self>]) -> Self::Requested;
+    fn send(&mut self, ops: &[Operation]) -> Self::Requested;
     fn receive(&mut self, buf: &mut Self::Buffer) -> Self::Response;
-    fn send_receive(&mut self, ops: &[Operation<Self>], buf: &mut Self::Buffer) -> Self::Response;
+    fn send_receive(&mut self, ops: &[Operation], buf: &mut Self::Buffer) -> Self::Response;
 }
+
+pub use gates::{PauliGate, HGate, SGate, TGate, CXGate};
+pub use operations::{Operation, OpsVec};
 
 #[cfg(test)]
 mod tests {
