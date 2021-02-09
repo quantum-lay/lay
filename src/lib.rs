49597b1c63c87e345393e70a0354e46491d6bbcd
gyu-don <takumi.kt+git@gmail.com>
2021-02-09 23:23:04 +0900

    make default implement for send_receive

--- src/lib.rs
+++ src/lib.rs
@@ -17,9 +17,16 @@ pub trait Layer {
     type Response;
 
     fn send(&mut self, ops: &[Self::Operation]) -> Self::Requested;
+
     fn receive(&mut self, buf: &mut Self::Buffer) -> Self::Response;
-    fn send_receive(&mut self, ops: &[Self::Operation], buf: &mut Self::Buffer) -> Self::Response;
+
+    fn send_receive(&mut self, ops: &[Self::Operation], buf: &mut Self::Buffer) -> Self::Response {
+        self.send(ops);
+        self.receive(buf)
+    }
+
     fn make_buffer(&self) -> Self::Buffer;
+
     fn opsvec(&self) -> OpsVec<Self> {
         OpsVec::new()
     }
